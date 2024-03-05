b7b78eebb14f925dd1455c621e1cabe9e8f8dd0a
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-05 10:35:41 +0500

    Add countdown timer functionality

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 "use strict";
+const startBtn = document.querySelector(".btn");
+const inputText = document.querySelector(".display");
+const bar = document.querySelector(".bar");
+
+startBtn.addEventListener("click", function () {
+  const intervalId = setInterval(() => {
+    let time = parseInt(inputText.value);
+    if (time > 0) {
+      time--;
+      inputText.value = time;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+
+  const time = parseInt(inputText.value);
+
+  const intervalId2 = setInterval(() => {
+    let barWidth = bar.getBoundingClientRect().width;
+    if (barWidth > 0) {
+      barWidth -= 300 / time / 200;
+      bar.style.width = `${barWidth}px`;
+    } else {
+      clearInterval(intervalId2);
+    }
+  }, 5);
+});
